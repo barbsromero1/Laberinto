f1eaca8fcfc8f1b9b7aa8e8d9e2256d1ed512904
Hans Labardini <57641368+Labardino@users.noreply.github.com>
2020-04-07 03:16:15 -0500

    Audio Mixer and Lives
    Se utilizó un Audio Mixer para poder cambiar el volumen del audio al igual que tener 2 canales.
    Tambén se utilizó un slider para la vida. Que aún así se tiene que hacer más bonito. Pero funcionan 4 vidas

---
+++ Assets/Scripts/UI/LivesManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class LivesManager : MonoBehaviour
+{
+    public int Totallives;
+    private int liveCounter;
+    private Text livesText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        livesText = GetComponent<Text>();
+        liveCounter = Totallives;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        livesText.text = "x " + liveCounter;
+    }
+    public void TakeLife()
+    {
+        liveCounter--;
+    }
+}
