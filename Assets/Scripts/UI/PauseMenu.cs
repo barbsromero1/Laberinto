9a3375f6c6fce7b8866b3d296c4a417315206a8f
barbsromero1 <35925256+barbsromero1@users.noreply.github.com>
2020-04-07 18:07:26 -0500

    Pause Menu
    added scripts, bottons with functions and coections to MainMennue scene

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static bool GamePause = false;
+    public GameObject pauseMenueUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePause)
+            {
+                resume();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+
+    public void resume()
+    {
+        pauseMenueUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePause = false;
+    }
+
+    void pause()
+    {
+        pauseMenueUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePause = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
